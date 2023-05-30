c9f820776611e4bad9bd1fea3ce55bd333e8de76
Pankaj Rawat <pankaj.rawat@sibingroup.com>
2023-05-30 22:55:53 +0530

    GST Calculation

--- src/app/modules/form/form.component.ts
+++ src/app/modules/form/form.component.ts
@@ -6,7 +6,7 @@ import { error } from 'console';
 import { ApiService } from 'src/app/services/api.service';
 
 
-  
+
 
 @Component({
   selector: 'app-form',
@@ -24,11 +24,18 @@ export class FormComponent implements OnInit {
   ];
   submitted = false;
   showErrorAlert = false;
-  errorMsg:string="Error: Please fill in all the required fields correctly.";
-  isFormSubmittedSuccessfully:boolean=false;
-  IsEditRequestedID:string="0";
-  EditFromData:any={};
-  constructor(private formBuilder: FormBuilder,private datePipe: DatePipe,private apiService: ApiService,private route: ActivatedRoute) { }
+  errorMsg: string = "Error: Please fill in all the required fields correctly.";
+  isFormSubmittedSuccessfully: boolean = false;
+  IsEditRequestedID: string = "0";
+  EditFromData: any = {};
+  ViewData:any={
+    daysdiff:0,
+    overallprice:0,
+    gst_percentile:0,
+    total_room_price:0,
+    room_price:0
+  }
+  constructor(private formBuilder: FormBuilder, private datePipe: DatePipe, private apiService: ApiService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.initializeForm();
@@ -36,7 +43,7 @@ export class FormComponent implements OnInit {
     this.route.params.subscribe(params => {
       this.IsEditRequestedID = params['id'];
       console.log("Fetch Booking")
-      if(this.IsEditRequestedID !="0"){
+      if (this.IsEditRequestedID != "0") {
         //IF Edit Requested
         this.GetBookingByBookingId();
       }
@@ -46,20 +53,20 @@ export class FormComponent implements OnInit {
   }
 
   initializeForm() {
-    console.log("Form Initialization",this.getCurrentDate());
+    console.log("Form Initialization", this.getCurrentDate());
 
     this.bookingForm = this.formBuilder.group({
-      guest_name: [this.IsEditRequestedID =="0"?'':this.EditFromData.data.guests.guest_name, [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
-      guest_email: [this.IsEditRequestedID =="0"?'':this.EditFromData.data.guests.guest_email, [Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]],
-      guest_phone: [this.IsEditRequestedID =="0"?'':this.EditFromData.data.guests.guest_phone, [Validators.required, Validators.pattern(/^\d{10}$/)]],
-      adults: [this.IsEditRequestedID =="0"?'':this.EditFromData.data.guests.adults, [Validators.required]],
+      guest_name: [this.IsEditRequestedID == "0" ? '' : this.EditFromData.data.guests.guest_name, [Validators.required, Validators.minLength(3), Validators.maxLength(15)]],
+      guest_email: [this.IsEditRequestedID == "0" ? '' : this.EditFromData.data.guests.guest_email, [Validators.required, Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)]],
+      guest_phone: [this.IsEditRequestedID == "0" ? '' : this.EditFromData.data.guests.guest_phone, [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      adults: [this.IsEditRequestedID == "0" ? '' : this.EditFromData.data.guests.adults, [Validators.required]],
       idproof: ['', [Validators.required]],
       idproofvalue: [{ value: '', disabled: true }, [Validators.required, Validators.pattern('')]],
-      childrens: [this.IsEditRequestedID =="0"?'':this.EditFromData.data.guests.childrens, [Validators.required]],
-      guest_address: [this.IsEditRequestedID =="0"?'':this.EditFromData.data.guests.guest_address, [Validators.required]],
-      check_out: [this.IsEditRequestedID =="0"?this.getCurrentDate():this.datePipe.transform(this.EditFromData.data.booking.booking_till,"yyyy-MM-dd"), Validators.required],
-      check_in: [this.IsEditRequestedID =="0"?this.getCurrentDate():this.datePipe.transform(this.EditFromData.data.booking.booking_from,"yyyy-MM-dd"), Validators.required],
-      roomCategory:[this.IsEditRequestedID =="0"?'':this.EditFromData.data.booking.room_category_id, [Validators.required]],
+      childrens: [this.IsEditRequestedID == "0" ? '' : this.EditFromData.data.guests.childrens, [Validators.required]],
+      guest_address: [this.IsEditRequestedID == "0" ? '' : this.EditFromData.data.guests.guest_address, [Validators.required]],
+      check_out: [this.IsEditRequestedID == "0" ? this.getCurrentDate() : this.datePipe.transform(this.EditFromData.data.booking.booking_till, "yyyy-MM-dd"), Validators.required],
+      check_in: [this.IsEditRequestedID == "0" ? this.getCurrentDate() : this.datePipe.transform(this.EditFromData.data.booking.booking_from, "yyyy-MM-dd"), Validators.required],
+      roomCategory: [this.IsEditRequestedID == "0" ? '' : this.EditFromData.data.booking.room_category_id, [Validators.required]],
 
     });
   }
@@ -75,7 +82,7 @@ export class FormComponent implements OnInit {
   }
 
   onSubmit() {
-    this.isFormSubmittedSuccessfully=false;
+    this.isFormSubmittedSuccessfully = false;
     this.submitted = true;
     if (this.bookingForm.invalid) {
       this.submitted = false;
@@ -91,11 +98,14 @@ export class FormComponent implements OnInit {
   // Handle form submission
   postBookingData() {
     if (this.bookingForm.valid) {
-      
+
       console.log("Form Valid !");
 
+      this.ViewData.daysdiff = this.CalculateNoOfDays();
+      this.ViewData.overallprice = this.OverAllPriceCalculation(Number(this.bookingForm.value.roomCategory), this.ViewData.daysdiff);
+
       //Initialising Data
-      let postObj: any ={
+      let postObj: any = {
         booking_id: 'asdasd',
         booking_date: new Date(),
         // booking_from: this.bookingForm.value.check_in,
@@ -114,26 +124,23 @@ export class FormComponent implements OnInit {
         guest_phone: this.bookingForm.value.guest_phone,
         guest_address: this.bookingForm.value.guest_address,
         guest_email: this.bookingForm.value.guest_email,
-        guest_total: Number(this.bookingForm.value.adults)+Number(this.bookingForm.value.childrens),
+        guest_total: Number(this.bookingForm.value.adults) + Number(this.bookingForm.value.childrens),
         adults: this.bookingForm.value.adults,
         childrens: this.bookingForm.value.childrens
       };
 
-      let noofdays = this.CalculateNoOfDays()
-
-
-      this.apiService.postData("Booking/OfflineBooking",postObj).subscribe((res:any)=>{
+      this.apiService.postData("Booking/OfflineBooking", postObj).subscribe((res: any) => {
         console.log("Response Success !");
-        this.isFormSubmittedSuccessfully=true;
+        this.isFormSubmittedSuccessfully = true;
       },
-      (error:any)=>{
-        this.isFormSubmittedSuccessfully=false;
-      })
+        (error: any) => {
+          this.isFormSubmittedSuccessfully = false;
+        })
       // Form is valid, proceed with saving data
 
 
     } else {
-      alert("InValid")
+      console.log("InValid Form Details")
       // Form is invalid, handle errors or display validation messages
     }
   }
@@ -147,30 +154,31 @@ export class FormComponent implements OnInit {
       idproof: '',
       idproofvalue: '',
       childrens: '',
-      check_out:this.getCurrentDate(),
-      check_in:this.getCurrentDate(),
-      roomCategory:'',
-      guest_address:''
+      check_out: this.getCurrentDate(),
+      check_in: this.getCurrentDate(),
+      roomCategory: '',
+      guest_address: ''
     });
     this.submitted = false;
     this.showErrorAlert = false;
-    this.isFormSubmittedSuccessfully=false;
+    this.isFormSubmittedSuccessfully = false;
     this.setValidationPattern();
   }
 
-  
+
   closeModal() {
     this.showModal = false;
   }
 
-  GetAllRoomCategories(){
-    this.apiService.getData("Common/GetAllRoomCategories").subscribe((res:any)=>{
+  GetAllRoomCategories() {
+    this.apiService.getData("Common/GetAllRoomCategories").subscribe((res: any) => {
       console.log("Response Success GetAllRoomCategories!");
       this.roomCategories = res.result;
-  })};
+    })
+  };
 
 
-  CalculateNoOfDays():Number{
+  CalculateNoOfDays(): number {
     const date1 = new Date(this.bookingForm.value.check_out);
     const date2 = new Date(this.bookingForm.value.check_in);
 
@@ -180,18 +188,29 @@ export class FormComponent implements OnInit {
     // Convert milliseconds to days
     const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 
-    console.log(daysDiff); 
+    console.log(daysDiff);
 
-    return daysDiff;
+    return daysDiff == 0 ? 1 : daysDiff;
   }
 
 
-  //TODO // Here You have to work
-  // GSTCalculation(room_category_id: number, noofdays: number) {
-  //   let room_price = Number(this.roomCategories.find(x => x.room_category_id === room_category_id)?.room_category_price);
-  //   let total_room_price = room_price * Number(noofdays);
-  //   let gst = total_room_price * 0.18;
-  // }
+  //TODO 
+  OverAllPriceCalculation(room_category_id: number, noofdays: number): any {
+    //Room Price
+    let room_price = Number(this.roomCategories.find(x => x.room_category_id === room_category_id)?.room_category_price);
+    this.ViewData.room_price=room_price;
+
+    //Total Price
+    let total_room_price = room_price * Number(noofdays);
+    this.ViewData.total_price=total_room_price;
+
+    //Gst Percentile
+    let gst = this.getGSTPercentage(total_room_price);
+    this.ViewData.gst_percentile=gst;
+
+    let overallprice = total_room_price * gst;
+    return overallprice;
+  }
 
 
 
@@ -214,23 +233,37 @@ export class FormComponent implements OnInit {
       idNumberControl.disable();
       idNumberControl.clearValidators();
     }
-  
+
     idNumberControl.updateValueAndValidity();
   }
 
 
-  async GetBookingByBookingId(){
-    this.EditFromData={};
-    await this.apiService.getDataById("Booking/GetBookingBySearchParam",this.IsEditRequestedID).subscribe((res:any)=>{
+  async GetBookingByBookingId() {
+    this.EditFromData = {};
+    await this.apiService.getDataById("Booking/GetBookingBySearchParam", this.IsEditRequestedID).subscribe((res: any) => {
       console.log("Edit : From Fetched Sucessfully !");
       this.EditFromData = res.result;
       this.initializeForm();
-    },(error:any)=>{
+    }, (error: any) => {
       this.initializeForm();
     })
   }
+
+  getGSTPercentage(price:number) {
+    let gstPercentage;
   
+    if (price <= 5000) {
+      gstPercentage = 5;
+    } else if (price <= 10000) {
+      gstPercentage = 12;
+    } else {
+      gstPercentage = 18;
+    }
   
-  
-  
+    return gstPercentage;
+  }
+
+
+
+
 }
